85a855e9f01c9ef7773d9a065d021394249c5685
Saket Saurabh <ssaurabh@adobe.com>
2015-02-11 12:44:15 +0530

    Creating a WebSocket server

--- websocket-server.js
+++ websocket-server.js
@@ -1,29 +1,26 @@
 module.exports = function(expressServer) {
 	
 	// task 1.1: create a WebSocket server	
-	
-	
-	
+	var ws = require('ws');
+	var webSocketServer = new ws.Server({ server: expressServer });
+	console.log('WebSocket server started.');
 	
 	// task 1.2: handle WebSocket server events
+	webSocketServer.on('connection', function(socket) {
+		console.log('Connection opened');
 	
-	    		    
+	  socket.on('close', function() {	
+			console.log('Connection closed');
+		});
 	
-	  
-	
-	
-	
-	
+		socket.on('message', function(message) {
+  		var sockets = webSocketServer.clients;	    	
+    	for (var i = 0; i < sockets.length; i++) {    
+		   	if (sockets[i] !== socket) {    
+		     		sockets[i].send(message);      
+		      }
+			}
+		});			 
 
-	
-  
-  
-  
-	
-	
-	
-	
-  
-
-	
-};
+	});
+}
